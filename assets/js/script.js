a3c5d8d08ed8c777d85260dec9c2678918de456f
Sara Zhou <sara@Saras-MacBook-Air.local>
2024-03-23 18:10:11 +1100

    add js code and some css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const weatherApiKey = 'ed3886039111ea00953ef8af2d692ba1';
 const searchFormEl = document.querySelector('#search-form');
 const searchInputEl = document.querySelector('#search-input');
 const todayContainerEl = document.querySelector('#today');
+const fivedayDivEl = document.querySelector('#five-day');
 const forecastContainerEl = document.querySelector('#forecast');
 const historyContainerEl = document.querySelector('#history');
 
@@ -17,6 +18,7 @@ const handleFormSubmit = function (event) {
     if (cityInput) {
         getCityWeather(cityInput);
         todayContainerEl.textContent = '';
+        fivedayDivEl.textContent = '';
         forecastContainerEl.textContent = '';
         searchInputEl.value = '';
     } else {
@@ -25,11 +27,8 @@ const handleFormSubmit = function (event) {
 };
 
 
-
-
-
 const getCityWeather = function (cityInput) {
-    let apiGeoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${weatherApiKey}`;
+    let apiGeoUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${cityInput}&limit=1&appid=${weatherApiKey}`;
 
     fetch(apiGeoUrl)
         .then(function (response) {
@@ -38,15 +37,28 @@ const getCityWeather = function (cityInput) {
                     .then(function (data) {
                         const lon = (data[0].lon);
                         const lat = (data[0].lat);
+                        getWeatherCurrent(lat, lon);
                         getWeatherForecast(lat, lon);
                     })
             } else { alert(`Error:${response.statusText}`) }
         }).catch(function (error) { alert('Unable to connect to openweathermap') })
 };
 
+const getWeatherCurrent = function (lat, lon) {
+    let apiCurrentUrl = `${weatherApiRootUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
+    fetch(apiCurrentUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        renderCurrent(data);
+                    })
+            } else { alert(`Error:${response.statusText}`) }
+        }).catch(function (error) { alert('Unable to connect to openweathermap') })
+};
 
 const getWeatherForecast = function (lat, lon) {
-    let apiForecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
+    let apiForecastUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
     fetch(apiForecastUrl)
         .then(function (response) {
             if (response.ok) {
@@ -56,23 +68,25 @@ const getWeatherForecast = function (lat, lon) {
                     })
             } else { alert(`Error:${response.statusText}`) }
         }).catch(function (error) { alert('Unable to connect to openweathermap') })
-}
+};
 
 const renderForecast = function (data) {
+    const forecastHeaderEl = document.createElement('h4');
+    forecastHeaderEl.textContent = '5-Day Forecast:';
+    fivedayDivEl.appendChild(forecastHeaderEl);
+
     for (let i = 0; i < data.list.length; i++) {
         if (i % 8 === 0) {
-            console.log(data.list[i]);
-            console.log(data.list[i].dt_txt);
-            console.log(data.list[i].weather[0].description);
-            
+
             const iconcode = data.list[i].weather[0].icon;
             const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            const iconAlt =data.list[i].weather[0].description;
+            const iconAlt = data.list[i].weather[0].description;
 
             const forecastCardsEl = document.createElement('div');
-            forecastCardsEl.setAttribute('class', 'card col m-2 bg-secondary text-light')
+            forecastCardsEl.setAttribute('class', 'card col m-2 text-light')
 
-            const forecastDateEl = document.createElement('h4');
+
+            const forecastDateEl = document.createElement('h5');
             const forecastIconEl = document.createElement('img');
             const forecastTempEl = document.createElement('p');
             const forecastWindEl = document.createElement('p');
@@ -81,7 +95,7 @@ const renderForecast = function (data) {
             forecastDateEl.textContent = data.list[i].dt_txt.slice(0, 10);
             forecastIconEl.setAttribute('src', iconUrl);
             forecastIconEl.setAttribute('alt', iconAlt);
-            forecastIconEl.setAttribute('width', '60');
+            forecastIconEl.setAttribute('width', '50');
             forecastTempEl.textContent = `Temp: ${data.list[i].main.temp} °C`;
             forecastWindEl.textContent = `Wind: ${data.list[i].wind.speed} m/s`;
             forecastHumidityEl.textContent = `Humidity: ${data.list[i].main.humidity}%`;
@@ -92,11 +106,39 @@ const renderForecast = function (data) {
             forecastCardsEl.appendChild(forecastWindEl);
             forecastCardsEl.appendChild(forecastHumidityEl);
             forecastContainerEl.appendChild(forecastCardsEl);
-
+            fivedayDivEl.appendChild(forecastContainerEl);
+            fivedayDivEl.setAttribute('class','mt-3');
         }
     }
-
 }
+
+const renderCurrent = function (data) {
+
+    const currentIconCode = data.weather[0].icon;
+    const currentIconUrl = "http://openweathermap.org/img/w/" + currentIconCode + ".png";
+    const currentIconEl = document.createElement('img');
+    currentIconEl.setAttribute('src', currentIconUrl);
+    currentIconEl.setAttribute('alt', data.weather[0].description);
+    currentIconEl.setAttribute('id','current-icon');
+
+    const currentCityEl = document.createElement('h3');
+    currentCityEl.textContent = data.name + ' (' + dayjs().format('YYYY/MM/DD')+')';
+    todayContainerEl.appendChild(currentCityEl);
+    todayContainerEl.appendChild(currentIconEl);
+
+    const currentTempEl = document.createElement('h6');
+    const currentWindEl = document.createElement('h6');
+    const currentHumidityEl = document.createElement('h6');
+    currentTempEl.textContent = `Temp: ${data.main.temp}°C`;
+    currentWindEl.textContent = `Wind: ${data.wind.speed} m/s`;
+    currentHumidityEl.textContent = `Humidity: ${data.main.humidity}%`;
+    todayContainerEl.appendChild(currentTempEl);
+    todayContainerEl.appendChild(currentWindEl);
+    todayContainerEl.appendChild(currentHumidityEl);
+    todayContainerEl.setAttribute('class','border border-2 border-secondary p-2');
+    
+
+};
 
 
 
